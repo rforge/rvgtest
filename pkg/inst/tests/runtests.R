c8784637529c7ae9b4ff99c9c153d5f3c84fdf7a
leydold <leydold@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-30 14:58:50 +0000

    - moved common auxiliary routines into helper file  - prepared runtests.R for using with R CMD check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rvgtest@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/tests/runtests.R
+++ pkg/inst/tests/runtests.R
@@ -3,7 +3,7 @@
 
 ## Load package 'testthat' 
 if(! require("testthat", quietly=TRUE)) {
-  message("\ncannot run unit tests -- package 'testthat' is not available\n")
+  message("\nCannot run unit tests -- package 'testthat' is not available!\n")
   quit(save="no",runLast=FALSE)
 }
 
@@ -16,35 +16,33 @@ opt.save <- options()
 ## Print warnings immediately
 ## options(warn=1)
 
-## --- Auxiliary routines ---------------------------------------------------
-
-## check for warning but suppress warning message
-expect_warning_suppress <- function (x, y) {
-        opt.store <- options()
-        options(warn=-1)
-        expect_warning(x,y)
-        options(opt.store)
+## Path to unit test files
+if (exists(".rvgt.RCMDCHECK") && isTRUE(.rvgt.RCMDCHECK)) {
+        ## Path to unit tests for 'R CMD check'
+        ## PKG.Rcheck/tests/../PKG/unitTests
+        ## Remark: variable '.rvgt.RCMDCHECK' has to be defined in
+        ##         the master test file for R CMD check
+        unittest.dir <- system.file(package="rvgtest", "tests")
+} else {
+        ## Path to unit tests for standalone running under Makefile (not R CMD check)
+        ## PKG/inst/tests
+        unittest.dir <- file.path(getwd())
 }
 
 ## --- Run tests ------------------------------------------------------------
 
-## Path to unit test files
-if(Sys.getenv("RCMDCHECK") == "FALSE") {
-        ## Path to unit tests for standalone running under Makefile (not R CMD check)
-        ## PKG/inst/tests
-        unittest.dir <- file.path(getwd())
-} else {
-        ## Path to unit tests for R CMD check
-        ## PKG.Rcheck/tests/../PKG/unitTests
-        unittest.dir <- system.file(package="rvgtest", "tests")
-}
+## Print header
+cat(rep("=",45),"\n",
+    " Run test suite in directory\n ",
+    unittest.dir,"\n",
+    rep("=",45),"\n", sep="")
 
 ## Use summary reporter
-test_dir(unittest.dir,reporter = "summary")
+test_dir(unittest.dir, reporter="summary")
 
 ## A possible alternative is the TAP reporter that uses
 ## the Test Anything Protocol (TAP) for the output result.
-## test_dir(".",reporter = "tap")
+## test_dir(unittest.dir, reporter="tap")
 
 ## --- End ------------------------------------------------------------------
 
